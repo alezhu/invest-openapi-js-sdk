ba9a5437e118fa1b4ad832827f381573642a66ca
Alexandr Zhuravlev <alexandr.zhuravlev@gmail.com>
2020-02-02 23:30:31 +0300

    Switch options-file for typedoc from js to json

--- typedoc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  excludeExternals: true,
-  entryPoint: './src/',
-  out: './doc/',
-  mode: 'file',
-  excludePrivate: true,
-  includeDeclarations: true,
-  plugin: [ 'typedoc-plugin-markdown','typedoc-plugin-no-inherit', 'typedoc-plugin-fixcomputednames']
-};
